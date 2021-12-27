fdb02645b175bb1edac1360fd167d83ad9ba94f7
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-12-27 14:06:21 -0500

    Created Foooter, adjusted home styling and linked to Landing page

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+// import image from "../public/GreenCityFooter.png"
+
+export default function Footer() {
+
+  return (
+    // <div className="bg-white w-screen  flex justify-end">
+    //   <img className="w-44" src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/870e7a0c-285d-4fd8-88c1-383fa1253000/public" />
+    // </div>
+    <div className="bg-teal-700 absolute bottom-0 w-full">
+      <img className="w-full" src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/a56e2f97-5d52-4cde-da74-9a33ac57a200/public" />
+    </div>
+  )
+}
