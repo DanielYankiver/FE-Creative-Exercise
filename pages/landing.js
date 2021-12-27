d64bd0d9357d13db7bbb6bde0c36b4da75cbe664
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-12-27 18:05:08 -0500

    minor updates

--- pages/landing.js
+++ pages/landing.js
@@ -21,9 +21,6 @@ export default function landing() {
           <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full" />
         </div>
       </div>
-      <div>
-        <img src="" />
-      </div>
     </div>
   )
 }
