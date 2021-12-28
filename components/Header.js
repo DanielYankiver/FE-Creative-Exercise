5352da6a9d2336ab8ffc10a3ba8853afcede4d3e
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-12-28 16:43:18 -0500

    Added Description box and hover effect

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <div className="w-full h-16  flex justify-between items-center">
-      <img src="/topl-Ribn-icon-large.png" className="w-12 ml-4"/>
+      <img src="/topl-Ribn-icon-large.png" className="w-12 ml-4 cursor-pointer"/>
       <Link href="/">
-        <button className="text-teal-50 bg-teal-500 w-32 h-8 rounded-md mr-4">
+        <button className="text-teal-50 bg-teal-500 w-32 h-8 rounded-md mr-4 cursor">
           Logout
         </button>
       </Link>
--- pages/landing.js
+++ pages/landing.js
@@ -10,13 +10,23 @@ export default function landing() {
       <div> 
         <Header />
       </div>
-      <div className=" flex justify-center content-center z-10">
+      <div className="flex justify-center content-center z-10">
         <div className="flip-image absolute top-20 hidden md:block">
           <img src="../cloudz.gif" className="md:w-screen"/>
         </div>
-        <div className="md:spin z-30">
-          <div className="spin w-96 mt-8 md:mt-24">
-            <img src="../topl-Ribn-icon-large.png" className="w-full"  />
+        <div className="md:spin z-30 cursor-pointer">
+          <div className="spin w-96 mt-8 md:mt-6 flex justify-center">
+            <img src="../topl-Ribn-icon-large.png" className="w-5/6"  />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center text-white mt-20 cursor-pointer">
+        <div className="border-2 rounded-lg w-1/4 flex-col justify-center hover:bg-white hover:border-teal-500 hover:text-teal-700">
+          <div className="text-center text-semibold text-4xl mt-10 mb-4">
+            Ribn Wallet
+          </div>
+          <div className="text-center text-lg mb-10">
+            {'Topl\'s blockchain wallet for tracking, tokenizing, and transaction impact. '}
           </div>
         </div>
       </div>
