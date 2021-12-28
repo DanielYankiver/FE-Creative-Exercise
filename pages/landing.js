6bd39e607fb33c6008452d2021fa73b1110bddd8
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-12-28 10:39:44 -0500

    Changed logo animation to onHover

--- pages/landing.js
+++ pages/landing.js
@@ -9,16 +9,18 @@ export default function landing() {
         <Header />
       </div>
       <div className=" flex justify-center content-center">
-        <div className="spin w-96 mt-8 md:mt-24">
-          <img src="../topl-Ribn-icon-large.png" className=""  />
+        <div className="hover:spin">
+          <div className="spin w-96 mt-8 md:mt-24">
+            <img src="../topl-Ribn-icon-large.png" className=""  />
+          </div>
         </div>
       </div>
       <div className=''>
         <div className="absolute bottom-0">
-          <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full" />
+          <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full"/>
         </div>
         <div className="flip-image absolute bottom-0 right-0">
-          <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full" />
+          <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full hidden md:block"/>
         </div>
       </div>
     </div>
