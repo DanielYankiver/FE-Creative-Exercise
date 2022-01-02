3ee4f4259c9ee9b20144ea902d985a094d0c05cf
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-01-02 15:47:14 -0600

    Cleaned up code

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 import { QuestionMarkCircleIcon, EyeIcon} from '@heroicons/react/solid'
 import Footer from '../components/Footer'
 
-
 export default function Home() {
+
   return (
-    <div className='w-full h-screen bg-teal-700'>
-      <div className='flex justify-center shadow-lg '>
-        <div className= 'md:bg-teal-600 rounded-lg mt-20 md:mt-36 lg:mt-8 w-11/12 md:w-1/2 lg:w-1/4 flex-col align-middle'>
-          <div className='flex justify-center mt-8 animate-bounce'>
-            <img src='./topl-Ribn-icon-large.png' className='w-16' />
+    <div className="w-full h-screen bg-teal-700">
+      <div className="flex justify-center shadow-lg">
+        <div className= "md:bg-teal-600 rounded-lg mt-20 md:mt-36 lg:mt-8 w-11/12 md:w-1/2 lg:w-1/4 flex-col align-middle">
+          <div className="flex justify-center mt-8 animate-bounce">
+            <img src="./topl-Ribn-icon-large.png" className="w-16"/>
           </div>
-          <div className='text-teal-50 text-3xl font-medium text-center mt-4 lg:mt-2 mb-1'>Ribn Wallet</div>
-          <div className='text-medium font-medium text-teal-50 text-center mx-2 md:mx-12 lg:mx-8 xl:mx-4'>
-            {'Topl\'s blockchain wallet for tracking, tokenizing, and transaction impact. '}
+          <div className="text-teal-50 text-3xl font-medium text-center mt-4 lg:mt-2 mb-1">Ribn Wallet</div>
+          <div className="text-medium font-medium text-teal-50 text-center mx-2 md:mx-12 lg:mx-8 xl:mx-4">
+            {"Topl\'s blockchain wallet for tracking, tokenizing, and transaction impact."}
           </div>
-          <form className=' mx-4 md:mx-8 my-10'>
-            <div className='mb-4'>
-              <div className='flex'>
-                <label className='text-medium text-teal-50 font-medium mb-2' htmlFor="username">
+          <form className="mx-4 md:mx-8 my-10">
+            <div className="mb-4">
+              <div className="flex">
+                <label className="text-medium text-teal-50 font-medium mb-2" htmlFor="username">
                   Enter Wallet Password
                 </label>
-                <QuestionMarkCircleIcon className='h-3 w-3 text-teal-100 mt-2 ml-1'/>    
+                <QuestionMarkCircleIcon className="h-3 w-3 text-teal-100 mt-2 ml-1"/>    
               </div> 
-              <div className='relative w-full'>
-                <div className='absolute inset-y-0 right-0 flex items-center px-2'>
-                  <EyeIcon className='h-4 w-4 text-teal-600 mt-1 ml-1'/> 
+              <div className="relative w-full">
+                <div className="absolute inset-y-0 right-0 flex items-center px-2">
+                  <EyeIcon className="h-4 w-4 text-teal-600 mt-1 ml-1"/> 
                 </div>
-                <input className='text-sm justify-between border rounded-md w-full h-9 py-2 px-3 bg-teal-50 text-gray-300 focus:outline-teal-500' id="username" type="password" placeholder='Type something...' />
+                <input className="text-sm justify-between border rounded-md w-full h-9 py-2 px-3 bg-teal-50 text-gray-300 focus:outline-teal-500" id="username" type="password" placeholder='Type something...' />
               </div>
             </div>
-            <div className='mt-8 tracking-wide'>
+            <div className="mt-8 tracking-wide">
               <Link href="/landing">
-                <button className='bg-teal-500 text-white text-xl font-medium w-full h-10 rounded-md'>CONTINUE</button>
+                <button className="bg-teal-500 text-white text-xl font-medium w-full h-10 rounded-md">CONTINUE</button>
               </Link>
-              <button className='bg-teal-100 text-teal-500 text-xl font-medium w-full h-10 rounded-md mt-4'>RESTORE WALLET</button>
+              <button className="bg-teal-100 text-teal-500 text-xl font-medium w-full h-10 rounded-md mt-4">RESTORE WALLET</button>
             </div>
-            <span className='md:text-lg text-teal-50 flex justify-center mt-4 '>Need Help? Contact <a href='https://www.google.com/' className='text-teal-200 pl-1 '>Ribn Support</a></span>
+            <span className="md:text-lg text-teal-50 flex justify-center mt-4 ">Need Help? Contact <a href="https://www.google.com/" className="text-teal-200 pl-1 ">Ribn Support</a></span>
           </form>
         </div>
       </div>
--- pages/landing.js
+++ pages/landing.js
@@ -1,13 +1,14 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { useEffect, useState } from 'react';
+import Footer2 from '../components/Footer2';
 
 export default function landing() {
 
   
   return (
-    <div className='landing'>
-      <div className='w-full h-screen bg-teal-700'>
+    <div className="landing">
+      <div className="w-full h-screen bg-teal-700">
         <Header />
         <div className="flex justify-center content-center z-10">
           <div className="flip-image absolute top-20">
@@ -15,28 +16,21 @@ export default function landing() {
           </div>
           <div className="spin z-30 cursor-pointer">
             <div className="md:w-96 mt-8 md:mt-6 flex justify-center">
-              <img src="../topl-Ribn-icon-large.png" className="w-36 md:w-60 lg:w-5/6"  />
+              <img src="../topl-Ribn-icon-large.png" className="w-44 md:w-60 lg:w-5/6"/>
             </div>
           </div>
         </div>
-        <div className="flex justify-center text-white mt-8 lg:mt-20 cursor-pointer">
-          <div className="border-2 rounded-lg w-10/12 md:w-1/2 lg:w-1/4 flex-col justify-center hover:bg-white hover:border-teal-500 hover:text-teal-700">
-            <div className="text-center text-semibold text-3xl md:text-4xl mt-6 md:mt-10 mb-4">
+        <div className="flex justify-center text-white mt-0 md:mt-10 lg:mt-20 cursor-pointer">
+          <div className="md:border-2 rounded-lg w-10/12 md:w-1/2 lg:w-1/4 flex-col justify-center hover:bg-white hover:border-teal-500 hover:text-teal-700">
+            <div className="text-center text-semibold text-4xl mt-6 md:mt-10 mb-4">
               Ribn Wallet
             </div>
-            <div className="text-center text-md md:text-lg mb-6 md:mb-10">
-              {'Topl\'s blockchain wallet for tracking, tokenizing, and transaction impact. '}
+            <div className="text-center text-lg mb-6 md:mb-10">
+              {"Topl\'s blockchain wallet for tracking, tokenizing, and transaction impact."}
             </div>
           </div>
         </div>
-        <div className="z-20">
-          <div className="absolute bottom-0">
-            <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full"/>
-          </div>
-          <div className="flip-image absolute bottom-0 right-0">
-            <img src="https://imagedelivery.net/nADYkEXSJPd0UjYrv7RXLw/839cfbf9-e1e0-4781-5ba3-b95963451e00/public" className="w-full hidden md:block"/>
-          </div>
-        </div>
+        <Footer2 />
       </div>
     </div>
   )
